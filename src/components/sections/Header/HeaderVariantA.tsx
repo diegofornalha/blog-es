f3ea06c4b3bbc3253ae446958a79a44e85339e06
Diego Fornalha <diegofornalha@gmail.com>
2023-06-13 18:40:27 -0300

    Update HeaderVariantA.tsx

--- src/components/sections/Header/HeaderVariantA.tsx
+++ src/components/sections/Header/HeaderVariantA.tsx
@@ -9,60 +9,58 @@ import WalletModal from './WalletModal';
 import classNames from 'classnames';
 
 export default function HeaderVariantA(props) {
-    const { title, isTitleVisible, logo, primaryLinks = [], secondaryLinks = [] } = props;
-    const [isModalOpen, setIsModalOpen] = React.useState(false);
-
-    const handleConnect = () => {
-        setIsModalOpen(true);
-    };
-
-    return (
-        <div className="flex items-center relative">
-            <SiteLogoLink title={title} isTitleVisible={isTitleVisible} logo={logo} />
-            {LocaleSelector()}
-            {primaryLinks.length > 0 && (
-                <ul className="hidden lg:flex lg:items-center mr-8 space-x-8" data-sb-field-path=".primaryLinks">
-                    <ListOfLinks links={primaryLinks} inMobileMenu={false} />
-                </ul>
-            )}
-            {secondaryLinks.length > 0 && (
-                <ul className="hidden lg:flex lg:items-center ml-auto space-x-8" data-sb-field-path=".secondaryLinks">
-                    <ListOfLinks links={secondaryLinks} inMobileMenu={false} />
-                    <ModeSwitcher />
-                </ul>
-            )}
-            <ConnectButton />
-            <WalletModal /> 
-        </div>
-    );
+ const { title, isTitleVisible, logo, primaryLinks = [], secondaryLinks = [] } = props;
+ const [isModalOpen, setIsModalOpen] = React.useState(false);
+ const handleConnect = () => {
+ setIsModalOpen(true);
+ };
+ return (
+ <div className="flex items-center relative">
+ <SiteLogoLink title={title} isTitleVisible={isTitleVisible} logo={logo} />
+ {LocaleSelector()}
+ {primaryLinks.length > 0 && (
+ <ul className="hidden lg:flex lg:items-center mr-8 space-x-8" data-sb-field-path=".primaryLinks">
+ <ListOfLinks links={primaryLinks} inMobileMenu={false} />
+ </ul>
+ )}
+ {secondaryLinks.length > 0 && (
+ <ul className="hidden lg:flex lg:items-center ml-auto space-x-8" data-sb-field-path=".secondaryLinks">
+ <ListOfLinks links={secondaryLinks} inMobileMenu={false} />
+ <ModeSwitcher />
+ </ul>
+ )}
+ <ConnectButton />
+ <WalletModal /> 
+ </div>
+ );
 }
 
 function SiteLogoLink({ title, isTitleVisible, logo }) {
-    if (!(logo || (title && isTitleVisible))) {
-        return null;
-    }
-    return (
-        <div className="mr-8">
-            <Link href="/" aria-label={title} className="sb-header-logo flex items-center">
-                {logo && <ImageBlock {...logo} className={classNames('max-h-12', { 'mr-2': isTitleVisible })} data-sb-field-path=".logo" />}
-                {title && isTitleVisible && (
-                    <span className="text-2xl uppercase font-medium" data-sb-field-path=".title">
-                        {title}
-                    </span>
-                )}
-            </Link>
-        </div>
-    );
+ if (!(logo || (title && isTitleVisible))) {
+ return null;
+ }
+ return (
+ <div className="mr-8">
+ <Link href="/" aria-label={title} className="sb-header-logo flex items-center">
+ {logo && <ImageBlock {...logo} className={classNames('max-h-12', { 'mr-2': isTitleVisible })} data-sb-field-path=".logo" />}
+ {title && isTitleVisible && (
+ <span className="text-2xl uppercase font-medium" data-sb-field-path=".title">
+ {title}
+ </span>
+ )}
+ </Link>
+ </div>
+ );
 }
 
 function ListOfLinks({ links, inMobileMenu }) {
-    return links.map((link, index) => (
-        <li key={index}>
-            {link.dropdownLinks ? (
-                <DropdownLink {...link} inMobileMenu={inMobileMenu} data-sb-field-path={`.${index}`} />
-            ) : (
-                <Action {...link} className={classNames(inMobileMenu && link.type === 'Button' ? 'w-full' : '')} data-sb-field-path={`.${index}`} />
-            )}
-        </li>
-    ));
+ return links.map((link, index) => (
+ <li key={index}>
+ {link.dropdownLinks ? (
+ <DropdownLink {...link} inMobileMenu={inMobileMenu} data-sb-field-path={`.${index}`} />
+ ) : (
+ <Action {...link} className={classNames(inMobileMenu && link.type === 'Button' ? 'w-full' : '')} data-sb-field-path={`.${index}`} />
+ )}
+ </li>
+ ));
 }
