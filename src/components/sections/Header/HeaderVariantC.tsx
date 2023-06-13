78fe63433934f8f772e1c5b0f8e7c8a9290cffcc
Diego Fornalha <diegofornalha@gmail.com>
2023-06-13 11:48:51 -0300

    Update HeaderVariantC.tsx

--- src/components/sections/Header/HeaderVariantC.tsx
+++ src/components/sections/Header/HeaderVariantC.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import SiteLogoLink from './SiteLogoLinkA';
-import ListOfLinks from './ListOfLinks';
-import ConnectButton from './ConnectButton';
+import * as React from 'react';
+import { Link, Action } from '../../atoms';
+import ImageBlock from '../../molecules/ImageBlock';
+import DropdownLink from './DropdownLink';
+import LocaleSelector from './LocaleSelector';
 import ModeSwitcher from './ModeSwitcher';
+import ConnectButton from './ConnectButton';
+import WalletModal from './WalletModal';
 
-const HeaderVariantC = ({ title, isTitleVisible, logo, primaryLinks = [], secondaryLinks = [] }) => {
+export default function HeaderVariantC(props) {
+    const { title, isTitleVisible, logo, primaryLinks = [], secondaryLinks = [] } = props;
+    const [isModalOpen, setIsModalOpen] = React.useState(false);
+    const handleConnect = () => {
+        setIsModalOpen(true);
+    };
     return (
         <div className="flex items-center relative">
             <SiteLogoLink title={title} isTitleVisible={isTitleVisible} logo={logo} />
+            {LocaleSelector()}
             {primaryLinks.length > 0 && (
                 <ul className="hidden lg:flex lg:items-center ml-auto space-x-8" data-sb-field-path=".primaryLinks">
                     <ListOfLinks links={primaryLinks} inMobileMenu={false} />
@@ -22,9 +31,38 @@ const HeaderVariantC = ({ title, isTitleVisible, logo, primaryLinks = [], second
                     <ModeSwitcher />
                 </ul>
             )}
-            <ConnectButton /> {/* Adicione o botão ConnectButton aqui */}
+            <ConnectButton onClick={handleConnect} /> {/* Adicione o manipulador de clique aqui */}
+            <WalletModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} /> {/* Adicione as props isOpen e onClose aqui */}
         </div>
     );
-};
+}
 
-export default HeaderVariantC;
+function SiteLogoLink({ title, isTitleVisible, logo }) {
+    if (!(logo || (title && isTitleVisible))) {
+        return null;
+    }
+    return (
+        <div className="mr-8">
+            <Link href="/" aria-label={title} className="sb-header-logo flex items-center">
+                {logo && <ImageBlock {...logo} className={classNames('max-h-12', { 'mr-2': isTitleVisible })} data-sb-field-path=".logo" />}
+                {title && isTitleVisible && (
+                    <span className="text-2xl uppercase font-medium" data-sb-field-path=".title">
+                        {title}
+                    </span>
+                )}
+            </Link>
+        </div>
+    );
+}
+
+function ListOfLinks({ links, inMobileMenu }) {
+    return links.map((link, index) => (
+        <li key={index}>
+            {link.dropdownLinks ? (
+                <DropdownLink {...link} inMobileMenu={inMobileMenu} data-sb-field-path={`.${index}`} />
+            ) : (
+                <Action {...link} className={classNames(inMobileMenu && link.type === 'Button' ? 'w-full' : '')} data-sb-field-path={`.${index}`} />
+            )}
+        </li>
+    ));
+}
