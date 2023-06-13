c5f59b719fd9ad8f5a8337c2c57dd9e7b9c40d7d
Diego Fornalha <diegofornalha@gmail.com>
2023-06-13 14:43:52 -0300

    Update WalletModal.tsx

--- src/components/sections/Header/WalletModal.tsx
+++ src/components/sections/Header/WalletModal.tsx
@@ -2,34 +2,34 @@ import * as fcl from "@onflow/fcl";
 import { useState } from "react";
 
 const WalletModal = () => {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    const handleConnect = async () => {
-        try {
-            await fcl.authenticate();
-            const { addr } = await fcl.currentUser().snapshot();
-            setUser(addr);
-        } catch (error) {
-            console.error("Failed to connect to wallet", error);
-        }
-    };
+  const handleConnect = async () => {
+    try {
+      await fcl.authenticate();
+      const { addr } = await fcl.currentUser().snapshot();
+      setUser(addr);
+    } catch (error) {
+      console.error("Failed to connect to wallet", error);
+    }
+  };
 
-    const disconnectWallet = async () => {
-        try {
-            await fcl.unauthenticate();
-            setUser(null);
-        } catch (error) {
-            console.error("Failed to disconnect from wallet", error);
-        }
-    };
+  const disconnectWallet = async () => {
+    try {
+      await fcl.unauthenticate();
+      setUser(null);
+    } catch (error) {
+      console.error("Failed to disconnect from wallet", error);
+    }
+  };
 
-    return (
-        <div>
-            <button onClick={handleConnect}>Connect to Flow Wallet</button>
-            {user && <p>Connected to: {user}</p>}
-            <button onClick={disconnectWallet}>Disconnect Wallet</button>
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={handleConnect}>Connect to Flow Wallet</button>
+      {user && <p>Connected to: {user}</p>}
+      <button onClick={disconnectWallet}>Disconnect Wallet</button>
+    </div>
+  );
 };
 
 export default WalletModal;
