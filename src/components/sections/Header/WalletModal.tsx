e430aa2d323d67231a8e9605b1cb64d15c2cb5e5
Diego Fornalha <diegofornalha@gmail.com>
2023-06-13 01:40:39 -0300

    Update WalletModal.tsx

--- src/components/sections/Header/WalletModal.tsx
+++ src/components/sections/Header/WalletModal.tsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as fcl from "@onflow/fcl";
 
 const WalletModal = () => {
+    const [authStatus, setAuthStatus] = useState("not connected");
+
     const handleConnect = async () => {
+        setAuthStatus("connecting");
         try {
             await fcl.authenticate();
+            setAuthStatus("connected");
         } catch (error) {
             console.error("Failed to connect to wallet", error);
+            setAuthStatus("not connected");
         }
     };
 
+    const handleDisconnect = async () => {
+        try {
+            await fcl.unauthenticate();
+            setAuthStatus("not connected");
+        } catch (error) {
+            console.error("Failed to disconnect from wallet", error);
+        }
+    };
+
+    const buttonText = {
+        "not connected": "Connect to Flow Wallet",
+        "connecting": "Connecting...",
+        "connected": "Disconnect"
+    }[authStatus];
+
     return (
         <div>
-            <button onClick={handleConnect}>Connect to Flow Wallet</button>
+            {authStatus !== "connected" ? (
+                <button onClick={handleConnect}>{buttonText}</button>
+            ) : (
+                <button onClick={handleDisconnect}>{buttonText}</button>
+            )}
         </div>
     );
 };
