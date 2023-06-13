85efea1ac8e4fc4d59e3dc5ab5e3103d5d31db79
Diego Fornalha <diegofornalha@gmail.com>
2023-06-13 12:19:50 -0300

    Update ListOfLinks.tsx

--- src/components/sections/Header/ListOfLinks.tsx
+++ src/components/sections/Header/ListOfLinks.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
+import * as React from 'react';
 import { Action } from '../../atoms';
 import DropdownLink from './DropdownLink';
 
-const ListOfLinks = ({ links, inMobileMenu }) => {
+export default function ListOfLinks({ links, inMobileMenu }) {
     return links.map((link, index) => (
         <li key={index}>
             {link.dropdownLinks ? (
                 <DropdownLink {...link} inMobileMenu={inMobileMenu} data-sb-field-path={`.${index}`} />
             ) : (
-                    <Action {...link} className={classNames(inMobileMenu && link.type === 'Button' ? 'w-full' : '')} data-sb-field-path={`.${index}`} />
-                )}
+                <Action {...link} className={classNames(inMobileMenu && link.type === 'Button' ? 'w-full' : '')} data-sb-field-path={`.${index}`} />
+            )}
         </li>
     ));
-};
-
-export default ListOfLinks;
+}
