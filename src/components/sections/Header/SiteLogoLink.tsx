867a489dbbbf63724e3cbbaca9a731934383c21a
Diego Fornalha <diegofornalha@gmail.com>
2023-06-13 12:17:26 -0300

    Create SiteLogoLink.tsx

---
+++ src/components/sections/Header/SiteLogoLink.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Link } from '../../atoms';
+import ImageBlock from '../../molecules/ImageBlock';
+
+export default function SiteLogoLink({ title, isTitleVisible, logo }) {
+    if (!(logo || (title && isTitleVisible))) {
+        return null;
+    }
+    return (
+        <div className="mr-8">
+            <Link href="/" aria-label={title} className="sb-header-logo flex items-center">
+                {logo && <ImageBlock {...logo} className={classNames('max-h-12', { 'mr-2': isTitleVisible })} data-sb-field-path=".logo" />}
+                {title && isTitleVisible && (
+                    <span className="text-2xl uppercase font-medium" data-sb-field-path=".title">
+                        {title}
+                    </span>
+                )}
+            </Link>
+        </div>
+    );
+}
